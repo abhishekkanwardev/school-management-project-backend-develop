ea567d9531e6033410bf0ab11b7ece2757690fd7
Necmi <necmi.1898@gmail.com>
2023-01-19 01:12:17 +0300

    settings changed and  some model register removed from admin where is used tests

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import User, AdminProfile, PrincipalProfile, TeacherProfile, StudentProfile, GuardianProfile
+from .models import User, AdminProfile, PrincipalProfile, TeacherProfile
 
 admin.site.register(User)
 admin.site.register(AdminProfile)
 admin.site.register(PrincipalProfile)
 admin.site.register(TeacherProfile)
-admin.site.register(StudentProfile)
-admin.site.register(GuardianProfile)
